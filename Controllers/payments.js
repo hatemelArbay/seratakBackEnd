13bcf9d364a29df81fcb137b2c478a369fbf97dd
hatemelArbay <78109132+hatemelArbay@users.noreply.github.com>
2025-08-13 21:22:21 +0300

    Updated payment redirect

--- Controllers/payments.js
+++ Controllers/payments.js
@@ -15,7 +15,7 @@ module.exports.initiatePayment= async(req , res )=>{
       cart_description: service.serviceTitle,
       customer_details: customer,
       // return: `${process.env.BASE_URL}/paymentSuccess`,
-      return: `https://seratak.netlify.app/paymentSuccess`,
+      return: `https://lawngreen-stork-810699.hostingersite.com/paymentSuccess.html`,
       
     };
     const data = await paymentService.createPaymentPage(orderData);
